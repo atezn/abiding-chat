aa8f20e01aeda2d39fa0da9f5009387d98435320
batuhan <batuislen@gmail.com>
2025-06-14 23:30:29 +0300

    connection var ama ses sikinti

---
+++ P2P VoiceChat Latest Trial/P2P VoiceChat Latest Trial/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace P2P_VoiceChat_Latest_Trial
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            startLoopBackButton = new Button();
+            stopLoopBackButton = new Button();
+            stunBox = new Button();
+            stunIPTxt = new TextBox();
+            stunPortTxt = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            targetIPBox = new TextBox();
+            targetPortBox = new TextBox();
+            startSendingButton = new Button();
+            stopSendingButton = new Button();
+            startReceivingButton = new Button();
+            stopReceivingButton = new Button();
+            SuspendLayout();
+            // 
+            // startLoopBackButton
+            // 
+            startLoopBackButton.Location = new Point(407, 28);
+            startLoopBackButton.Name = "startLoopBackButton";
+            startLoopBackButton.Size = new Size(108, 25);
+            startLoopBackButton.TabIndex = 0;
+            startLoopBackButton.Text = "start loopback";
+            startLoopBackButton.UseVisualStyleBackColor = true;
+            startLoopBackButton.Click += startLoopBackButton_Click;
+            // 
+            // stopLoopBackButton
+            // 
+            stopLoopBackButton.Location = new Point(407, 59);
+            stopLoopBackButton.Name = "stopLoopBackButton";
+            stopLoopBackButton.Size = new Size(108, 25);
+            stopLoopBackButton.TabIndex = 1;
+            stopLoopBackButton.Text = "stop loopback";
+            stopLoopBackButton.UseVisualStyleBackColor = true;
+            stopLoopBackButton.Click += stopLoopBackButton_Click;
+            // 
+            // stunBox
+            // 
+            stunBox.Location = new Point(12, 23);
+            stunBox.Name = "stunBox";
+            stunBox.Size = new Size(123, 39);
+            stunBox.TabIndex = 2;
+            stunBox.Text = "get STUN info";
+            stunBox.UseVisualStyleBackColor = true;
+            stunBox.Click += stunBox_Click;
+            // 
+            // stunIPTxt
+            // 
+            stunIPTxt.Location = new Point(176, 20);
+            stunIPTxt.Name = "stunIPTxt";
+            stunIPTxt.Size = new Size(100, 23);
+            stunIPTxt.TabIndex = 3;
+            stunIPTxt.Text = "STUN IP";
+            // 
+            // stunPortTxt
+            // 
+            stunPortTxt.Location = new Point(176, 49);
+            stunPortTxt.Name = "stunPortTxt";
+            stunPortTxt.Size = new Size(100, 23);
+            stunPortTxt.TabIndex = 4;
+            stunPortTxt.Text = "STUN Port";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(150, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(20, 15);
+            label1.TabIndex = 5;
+            label1.Text = "IP:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(141, 52);
+            label2.Name = "label2";
+            label2.Size = new Size(32, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Port:";
+            // 
+            // targetIPBox
+            // 
+            targetIPBox.Location = new Point(162, 171);
+            targetIPBox.Name = "targetIPBox";
+            targetIPBox.Size = new Size(124, 23);
+            targetIPBox.TabIndex = 7;
+            targetIPBox.Text = "Enter target IP";
+            // 
+            // targetPortBox
+            // 
+            targetPortBox.Location = new Point(162, 200);
+            targetPortBox.Name = "targetPortBox";
+            targetPortBox.Size = new Size(124, 23);
+            targetPortBox.TabIndex = 8;
+            targetPortBox.Text = "Enter target Port";
+            // 
+            // startSendingButton
+            // 
+            startSendingButton.Location = new Point(6, 159);
+            startSendingButton.Name = "startSendingButton";
+            startSendingButton.Size = new Size(124, 35);
+            startSendingButton.TabIndex = 11;
+            startSendingButton.Text = "start sending sound";
+            startSendingButton.UseVisualStyleBackColor = true;
+            startSendingButton.Click += startSendingButton_Click;
+            // 
+            // stopSendingButton
+            // 
+            stopSendingButton.Location = new Point(6, 209);
+            stopSendingButton.Name = "stopSendingButton";
+            stopSendingButton.Size = new Size(124, 35);
+            stopSendingButton.TabIndex = 12;
+            stopSendingButton.Text = "stop sending sound";
+            stopSendingButton.UseVisualStyleBackColor = true;
+            stopSendingButton.Click += stopSendingButton_Click;
+            // 
+            // startReceivingButton
+            // 
+            startReceivingButton.AccessibleDescription = "startReceivingSound";
+            startReceivingButton.Location = new Point(311, 159);
+            startReceivingButton.Name = "startReceivingButton";
+            startReceivingButton.Size = new Size(140, 35);
+            startReceivingButton.TabIndex = 13;
+            startReceivingButton.Text = "start receiving sound";
+            startReceivingButton.UseVisualStyleBackColor = true;
+            startReceivingButton.Click += startReceivingButton_Click;
+            // 
+            // stopReceivingButton
+            // 
+            stopReceivingButton.AccessibleDescription = "stopReceivingSound";
+            stopReceivingButton.Location = new Point(311, 210);
+            stopReceivingButton.Name = "stopReceivingButton";
+            stopReceivingButton.Size = new Size(140, 35);
+            stopReceivingButton.TabIndex = 14;
+            stopReceivingButton.Text = "stop receiving sound";
+            stopReceivingButton.UseVisualStyleBackColor = true;
+            stopReceivingButton.Click += stopReceivingButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(571, 266);
+            Controls.Add(stopReceivingButton);
+            Controls.Add(startReceivingButton);
+            Controls.Add(stopSendingButton);
+            Controls.Add(startSendingButton);
+            Controls.Add(targetPortBox);
+            Controls.Add(targetIPBox);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(stunPortTxt);
+            Controls.Add(stunIPTxt);
+            Controls.Add(stunBox);
+            Controls.Add(stopLoopBackButton);
+            Controls.Add(startLoopBackButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button startLoopBackButton;
+        private Button stopLoopBackButton;
+        private Button stunBox;
+        private TextBox stunIPTxt;
+        private TextBox stunPortTxt;
+        private Label label1;
+        private Label label2;
+        private TextBox targetIPBox;
+        private TextBox targetPortBox;
+        private Button startSendingButton;
+        private Button stopSendingButton;
+        private Button startReceivingButton;
+        private Button stopReceivingButton;
+    }
+}
---
+++ P2P VoiceChat Latest Trial/P2P VoiceChat Latest Trial/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Windows.Forms;
+using NAudio.Wave;
+using Concentus;
+using Concentus.Structs;
+using Concentus.Enums;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using STUN; // Correct namespace for STUNUtils
+
+namespace P2P_VoiceChat_Latest_Trial
+{
+    public partial class Form1 : Form
+    {
+
+        private WaveInEvent? waveIn;
+        private WaveOutEvent? waveOut;
+        private BufferedWaveProvider? bufferedWaveProvider;
+
+        private OpusEncoder? encoder;
+        private OpusDecoder? decoder;
+
+        private UdpClient? udpSender; // Declare UDP client if needed for future use
+        private UdpClient? udpReceiver; // Declare STUN client if needed for future use
+
+
+        private bool isLooping = false;
+        private bool isReceiving = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startLoopBackButton_Click(object sender, EventArgs e)
+        {
+            if (isLooping) // eger zaten calisiyorsa pop up mesaj
+            {
+                MessageBox.Show("Loopback is already running.");
+                return;
+            }
+
+            encoder = new OpusEncoder(48000, 1, OpusApplication.OPUS_APPLICATION_VOIP)
+            {   // encoder ve decoder baska sekilde declare edildiginde hata veriyor sadece bu sekilde calistirabildim
+                Bitrate = 64000
+            };
+
+            decoder = new OpusDecoder(48000, 1);
+
+            waveIn = new WaveInEvent
+            {
+                DeviceNumber = 0,
+                WaveFormat = new WaveFormat(48000, 16, 1),
+                BufferMilliseconds = 20
+            };
+
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+            waveIn.StartRecording();
+
+            bufferedWaveProvider = new BufferedWaveProvider(waveIn.WaveFormat);
+            waveOut = new WaveOutEvent();
+            waveOut.Init(bufferedWaveProvider);
+            waveOut.Play();
+
+            isLooping = true;
+
+        }
+
+
+
+        private void stopLoopBackButton_Click(object sender, EventArgs e)
+        {
+            if (!isLooping) // eger zaten calismiyorsa pop up mesaj
+            {
+                MessageBox.Show("Loopback is not running.");
+                return;
+            }
+
+            waveIn.StopRecording();
+            waveIn.Dispose();
+            waveIn = null;
+
+            waveOut.Stop();
+            waveOut.Dispose();
+            waveOut = null;
+
+            encoder.Dispose();
+            decoder.Dispose();
+
+            isLooping = false;
+
+        }
+
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (!isLooping) return;
+
+            short[] pcm = new short[e.BytesRecorded / 2];
+            Buffer.BlockCopy(e.Buffer, 0, pcm, 0, e.BytesRecorded);
+
+            byte[] encoded = new byte[4000];
+            int encodedLength = encoder.Encode(pcm, 0, pcm.Length, encoded, 0, encoded.Length);
+
+            short[] decodedPcm = new short[960 * 6];
+            int decodedSamples = decoder.Decode(encoded, 0, encodedLength, decodedPcm, 0, decodedPcm.Length, false);
+
+            byte[] decodedBytes = new byte[decodedSamples * 2];
+            Buffer.BlockCopy(decodedPcm, 0, decodedBytes, 0, decodedBytes.Length);
+
+            bufferedWaveProvider.AddSamples(decodedBytes, 0, decodedBytes.Length);
+        }
+
+
+        private void stunBox_Click(object sender, EventArgs e)
+        {
+            ///"stun.l.google.com:19302"; // Google STUN server  
+
+            if (!STUNClient.TryParseHostAndPort($"stun.schlund.de:3478", out IPEndPoint stunEndPoint))
+                throw new Exception("Failed to resolve STUN server address");
+
+            STUNClient.ReceiveTimeout = 500;
+            var queryResult = STUNClient.Query(stunEndPoint, STUNQueryType.ExactNAT, true); // Removed invalid parameter 'NATTypeDetectionRFC.Rfc5780'  
+
+            if (queryResult.QueryError != STUNQueryError.Success)
+                throw new Exception("Query Error: " + queryResult.QueryError.ToString());
+
+            stunIPTxt.Text = queryResult.PublicEndPoint.Address.ToString();
+            stunPortTxt.Text = queryResult.PublicEndPoint.Port.ToString();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startSendingButton_Click(object sender, EventArgs e)
+        {
+            string targetIp = targetIPBox.Text.Trim();
+            int targetPort = 5500; //int.Parse(targetPortBox.Text.Trim());
+            udpSender = new UdpClient();
+            IPEndPoint targetEndPoint = new IPEndPoint(IPAddress.Parse(targetIp), targetPort);
+
+            encoder = new OpusEncoder(48000, 1, OpusApplication.OPUS_APPLICATION_VOIP);
+            encoder.Bitrate = 64000; // Set bitrate for Opus encoder
+
+            waveIn = new WaveInEvent();
+            waveIn.DeviceNumber = 0;
+            waveIn.WaveFormat = new WaveFormat(48000, 16, 1); // mono, 48kHz
+            waveIn.BufferMilliseconds = 20;
+            waveIn.DataAvailable += (s, a) =>
+            {
+                short[] pcm = new short[a.BytesRecorded / 2];
+                Buffer.BlockCopy(a.Buffer, 0, pcm, 0, a.BytesRecorded);
+
+                byte[] encoded = new byte[4000];
+                int encodedLength = encoder.Encode(pcm, 0, pcm.Length, encoded, 0, encoded.Length);
+                
+                if (encodedLength < 0)
+                {
+                    MessageBox.Show("Encoding failed, length is negative.");
+                    return;
+                }
+                byte[] toSend = new byte[encodedLength];
+                Array.Copy(encoded, toSend, encodedLength);
+
+
+
+                udpSender.Send(toSend, toSend.Length, targetEndPoint);
+                //Console.WriteLine($"[SEND] Sending {toSend.Length} bytes to {targetEndPoint}");
+                MessageBox.Show($"Sending {toSend.Length} bytes to {targetEndPoint}");
+            };
+
+            waveIn.StartRecording();
+        }
+
+        private void stopSendingButton_Click(object sender, EventArgs e)
+        {
+            waveIn?.StopRecording();
+            waveIn?.Dispose();
+            waveIn = null;
+
+            udpSender?.Close();
+            udpSender = null;
+
+            encoder?.Dispose();
+            encoder = null;
+        }
+
+        private void startReceivingButton_Click(object sender, EventArgs e)
+        {
+            int listenPort = 5500; //int.Parse(targetPortBox.Text.Trim()); // aynı textbox kullanılabilir
+            udpReceiver = new UdpClient(listenPort);
+
+            decoder = new OpusDecoder(48000, 1);
+
+            bufferedWaveProvider = new BufferedWaveProvider(new WaveFormat(48000, 16, 1));
+            waveOut = new WaveOutEvent();
+            waveOut.Init(bufferedWaveProvider);
+            waveOut.Play();
+
+            isReceiving = true;
+            Task.Run(() =>
+            {
+                while (isReceiving)
+                {
+                    try
+                    {
+                        IPEndPoint remoteEP = null;
+                        byte[] received = udpReceiver.Receive(ref remoteEP);
+
+                        short[] decoded = new short[1920];
+                        int samplesDecoded = decoder.Decode(received, 0, received.Length, decoded, 0, decoded.Length, false);
+
+                        byte[] buffer = new byte[samplesDecoded * 2];
+                        Buffer.BlockCopy(decoded, 0, buffer, 0, buffer.Length);
+                        bufferedWaveProvider.AddSamples(buffer, 0, buffer.Length);
+                        MessageBox.Show($"Received {buffer.Length} bytes from {remoteEP}");
+                    }
+                    catch { }
+                }
+            });
+        }
+
+        private void stopReceivingButton_Click(object sender, EventArgs e)
+        {
+            isReceiving = false;
+
+            udpReceiver?.Close();
+            udpReceiver = null;
+
+            waveOut?.Stop();
+            waveOut?.Dispose();
+            waveOut = null;
+
+            decoder = null;
+        }
+    }
+}
